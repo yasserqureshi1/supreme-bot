74207c13d544acf1b091130e3c59a22689520709
Yasser Qureshi <yasser.q@hotmail.com>
2021-10-09 16:49:14 +0100

    Remove

--- instanceManager.py
+++
@@ -1,8 +0,0 @@
-import os
-from subprocess import *
-
-
-def create_instance(number_of_instances):
-    for i in range(number_of_instances):
-        p = Popen([r'SupremeBot.py', "ArcView"], shell=True, stdin=PIPE, stdout=PIPE)
-        output = p.communicate()
